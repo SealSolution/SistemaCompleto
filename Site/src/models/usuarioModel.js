0c736c56125deb5312886240f0b3d9e7043f8acb
ricardoVicentepc <Ricardo.vicente@sptech.school>
2023-05-31 16:12:43 -0300

    Mudando local cadastro empresa e ardamento da fk

--- Site/src/models/usuarioModel.js
+++ Site/src/models/usuarioModel.js
@@ -1,4 +1,7 @@
 var database = require("../database/config")
+var DadosEmail = null
+var DadosCnpj = null
+var emailDono = null
 
 
 function listar() {
@@ -9,8 +12,6 @@ function listar() {
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
-var DadosEmail = null
-var DadosCnpj = null
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
@@ -20,8 +21,8 @@ function entrar(email, senha) {
     return database.executar(instrucao);
 }
 function cadastrar(nome, email, cpf, senha) {
+    emailDono = email
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf);
-
     var instrucao = `
     INSERT INTO usuario (nomeUsuario, email, cpf, senha) VALUES ('${nome}', '${email}', '${cpf}' , '${senha}');
     `;
@@ -34,15 +35,23 @@ function cadastrar2(nomeEmpresa, cnpj, desc) {
     DadosEmail = nomeEmpresa
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente.");
 
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    // INSERT INTO empresa (nomeEmpresa, CNPJ, CEP, numero, rua, bairro, cidade, UF, complemento, descricao) VALUES ('${nome}', '${cnpj}', '${cep}', '${numero}', '${rua}', '${bairro}', '${cidade}', '${uf}', '${complemento}', '${desc}');
     var instrucao2 = `
     INSERT INTO empresa (nome, cnpj, descricao) VALUES ('${nomeEmpresa}', '${cnpj}', '${desc}');
     `;
 
+
+    var comp = `SET @company = (select idEmpresa from empresa where nome = 'Rick' and cnpj = '123');`
+    database.executar(comp)
+    var ownertoComp = `SET @ownerToCompany = (select idUsuario from usuario order by idUsuario desc limit 1);`
+    database.executar(ownertoComp)
+    var donoComEmp = `
+    update sealsolution.usuario set fkEmpresa = @company where idUsuario = @ownerToCompany;
+    `
+
     console.log("Executando a instrução SQL: \n" + instrucao2);
-    return database.executar(instrucao2);
+    
+    database.executar(instrucao2)
+    return  database.executar(donoComEmp);
 }
 function Funcionario(nomeFunc, funcaoFunc, nivelAcessoFunc, emailFunc, senhaFunc, cpfFunc) {
     var instrucao = `
@@ -50,11 +59,11 @@ function Funcionario(nomeFunc, funcaoFunc, nivelAcessoFunc, emailFunc, senhaFunc
     console.log("Executando a instrução SQL: \n" + instrucao);
     console.log(`EMail : ${DadosEmail} && cnpj: ${DadosCnpj}`)
     
-    var instrucao2 = `
+    var linkFuncComSuaEmpresa = `
     update usuario set fkEmpresa = (select idEmpresa from empresa where nome = '${DadosEmail}' and cnpj = '${DadosCnpj}') where idUsuario = 1;
     `;
 
-    return database.executar(instrucao2) , database.executar(instrucao);
+    return database.executar(linkFuncComSuaEmpresa) , database.executar(instrucao);
 }
 
 
